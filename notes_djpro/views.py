60b4e9a1a364f1c6193fd0fb12b0287a7399ba8b
unknown <Administrator@NEWLAB001.sigctry.com>
2025-02-21 14:40:02 +0530

    authenticate added

--- notes_djpro/views.py
+++ notes_djpro/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('note_list')
+    else:
+        form = UserCreationForm()
+    return render(request, 'notes/register.html', {'form': form})
+
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('note_list')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'notes/login.html', {'form': form})
+
+def user_logout(request):
+    logout(request)
+    return redirect('home')
+
+
